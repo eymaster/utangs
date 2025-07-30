077dcb64f4dbb27f8e40b80cb89328a792e1a5a1
eymaster <61962873+eymaster@users.noreply.github.com>
2025-07-30 16:40:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def index():
             new_debt = Debt(name=name, amount=float(amount), reason=reason)
             db.session.add(new_debt)
             db.session.commit
-            return redirect(url_for('index'))
+            # return redirect(url_for('index'))
             #debts.append({
             #    'name': name,
               #  'amount': float(amount),
