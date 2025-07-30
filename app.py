a4b3a33ee9dbe9e9acc0b899ec546771fbddd4c3
eymaster <61962873+eymaster@users.noreply.github.com>
2025-07-30 10:41:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-
-# SQLite (use DATABASE_URL for Render)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')  # Provided by Render
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Define the database model
+# Define database model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<User {self.name}>'
-
-# Home route with form
+# Routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
-        email = request.form['email']
-        new_user = User(name=name, email=email)
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/users')
-        except:
-            return "There was an issue adding your data"
+        user = User(name=name)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/users')
     return render_template('index.html')
 
-# Display all users
 @app.route('/users')
 def users():
     all_users = User.query.all()
     return render_template('users.html', users=all_users)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create tables
     app.run(debug=True)
