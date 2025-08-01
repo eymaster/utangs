1d0368193cfc483f8ffa538a167f5a1c4b635b3f
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-01 10:34:14 +0800

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,14 +1,41 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
-  e.waitUntil(
-    caches.open("utang-cache").then((cache) => {
-      return cache.addAll(["/", "/static/192.png", "/static/512.png"]);
+
+const CACHE_NAME = 'utang-manager-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/192.png',
+  '/static/icons/512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
