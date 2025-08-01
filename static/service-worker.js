572272a3286eeb48dad72dd9ab18cc55099e5398
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-01 10:53:24 +0800

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'utang-manager-v1';
 const urlsToCache = [
   '/',
   '/static/manifest.json',
-  '/static/icons/192.png',
-  '/static/icons/512.png',
+  '/static/192.png',
+  '/static/512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
