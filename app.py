403e78c4180a0a2a4aaa226f8b37b698d990f164
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-02 06:50:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ db = SQLAlchemy(app)
 #with app.app_context():
     #db.session.query(History).delete()
     #db.session.commit()
-with app.app_context():
-    db.session.execute('ALTER TABLE debt ADD COLUMN lender VARCHAR(100);')
-    db.session.commit()
+#with app.app_context():
+    #db.session.execute('ALTER TABLE debt ADD COLUMN lender VARCHAR(100);')
+    #db.session.commit()
 # Database model
 class Debt(db.Model):
     __tablename__ = 'debt'
