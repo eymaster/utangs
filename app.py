2c748687e8ac901f52e6c8564562a06f5e334916
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-03 00:00:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.orm import aliased
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -108,3 +109,10 @@ def split():
         db.session.commit()
 
     return jsonify({"success": True})
+
+
+# One-time init route to create tables
+@app.route('/initdb')
+def initdb():
+    db.create_all()
+    return "Database tables created nyeee."
