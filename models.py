414173173a98536d3e966f94dd81815eda7eb59c
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-02 23:42:58 +0800

    Update models.py

--- models.py
+++ models.py
@@ -18,5 +18,5 @@ class History(db.Model):
     timestamp = db.Column(db.DateTime)
 
 class Person(db.Model):
-id = db.Column(db.Integer, primary_key=True)
-name = db.Column(db.String(50), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
