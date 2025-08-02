45bde2472bdb35cd13df6056be7a6e5568339ec4
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-02 20:04:23 +0800

    Add files via upload

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,41 +1,20 @@
 
-const CACHE_NAME = 'utang-manager-v1';
-const urlsToCache = [
-  '/',
-  '/static/manifest.json',
-  '/static/192.png',
-  '/static/512.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('utang-app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/static/icons/icon-192.png',
+        '/static/icons/icon-512.png'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
